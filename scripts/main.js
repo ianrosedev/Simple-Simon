6468d4bc981d05f79b885ba740fcf4f3e94771c5
Ian Rose <ianrosedev@gmail.com>
2016-11-23 03:53:33 -0500

    dirty start to game

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,40 @@
-console.log('main script loaded');
+$(function() {
+
+  $('#test-bed').html('Testing 123');
+
+  const tiles = Object.freeze({
+    0: 'green',
+    1: 'red',
+    2: 'blue',
+    3: 'yellow'
+  });
+
+  function activeTile() {
+    return Math.floor(Math.random() * 4) + 0;
+  }
+
+
+  function colorsArray(times) {
+    let colors = [];
+    for (let i = 0; i < times; i++) {
+      colors[i] = tiles[activeTile()];
+    }
+    return colors;
+  }
+
+  function display(array = colorsArray(5)) {
+    console.log(array);
+    for (let i = 0; i <= array.length; i++) {
+      setTimeout(function() {
+        // !Why does i - 1 make this work?
+        $('#' + array[i - 1] + '-tile').addClass('lighten');
+      }, i * 1000);
+      setTimeout(function() {
+        $('#' + array[i - 1] + '-tile').removeClass('lighten');
+      }, i * 1200);
+    }
+  }
+
+  $('button').click(() => display());
+
+});
