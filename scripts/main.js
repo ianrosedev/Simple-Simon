dd6966951fe15dff54656d714f4fba134ec4241b
Ian Rose <ianrosedev@gmail.com>
2016-11-30 21:02:51 -0500

    finished timing

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,30 @@
 $(function() {
 
-  let MasterColors = [];
-  let UserColors = [];
+  let CpuColors = [],
+      UserColors = [],
+      timeWait = 1000,
+      timeFlash = 500,
+      speedUpEvery = 3;
 
   // Array of colors
   function colorsArray() {
     const colors = ['green', 'red', 'blue', 'yellow'];
 
     return (function() {
-      MasterColors.push(colors[Math.floor(Math.random() * 4)]);
-      return MasterColors;
+      CpuColors.push(colors[Math.floor(Math.random() * 4)]);
+      return CpuColors;
     })();
   }
 
   // Makes the tiles flash
   function display() {
     let array = colorsArray();
-    let timeWait = 1150;
-    let timeFlash = 500;
     let l = array.length;
 
-    if ((l + 1) % 7 === 0) {
-      timeWait -= 50;
-      timeFlash -= 5;
+    if (l > 1 && speedUpEvery < 16 && (l - 1) % speedUpEvery === 0) {
+      timeWait -= 200;
+      timeFlash -= 100;
+      speedUpEvery += 3;
     }
 
     for (let i = 0; i < l; i++) {
@@ -36,33 +38,69 @@ $(function() {
   }
 
   // User Input
-  $('#green-input').click(function() {UserColors.push('green');});
-  $('#red-input').click(function() {UserColors.push('red');});
-  $('#blue-input').click(function() {UserColors.push('blue');});
-  $('#yellow-input').click(function() {UserColors.push('yellow');});
+  $('#green-tile').on('mousedown', function() {
+    UserColors.push('green');
+    $(this).addClass('darken');
+    let self = this;
+    setTimeout(function() {
+      $(self).removeClass('darken');
+    }, 250);
+  });
+
+  $('#red-tile').on('mousedown', function() {
+    UserColors.push('red');
+    $(this).addClass('darken');
+    let self = this;
+    setTimeout(function() {
+      $(self).removeClass('darken');
+    }, 250);
+  });
+
+  $('#blue-tile').on('mousedown', function() {
+    UserColors.push('blue');
+    $(this).addClass('darken');
+    let self = this;
+    setTimeout(function() {
+      $(self).removeClass('darken');
+    }, 250);
+  });
+
+  $('#yellow-tile').on('mousedown', function() {
+    UserColors.push('yellow');
+    $(this).addClass('darken');
+    let self = this;
+    setTimeout(function() {
+      $(self).removeClass('darken');
+    }, 250);
+  });
 
   // User Submit
-  $('#submit-answer').click(function() {
-    $('#play-game').prop('disabled', false);
+  $('#submit-answer').on('click', function() {
     $(this).prop('disabled', true).addClass('hide');
+    $('#play-game').prop('disabled', false);
 
-    if (MasterColors.join('') !== UserColors.join('')) {
-      $('#game-result').html(`Sorry, It Was: ${MasterColors.join(', ')}`).addClass('red');
-      $('#user-input-answers').html(`Your Guess: ${UserColors.join(', ')}`);
+    if (CpuColors.join('') !== UserColors.join('')) {
+      $('#game-result').html(`Sorry, It Was: ${CpuColors.join(', ')}`).attr('class', 'red');
+      $('#user-input-answers').html(`Your Guess: ${UserColors.join(', ') || 'At least try to guess!'}`);
       $('#play-game').html('Play Again?').removeClass('hide');
-      MasterColors = [];
+
+      // Reset data
+      CpuColors = [];
+      timeWait = 1000,
+      timeFlash = 500,
+      speedUpEvery = 3;
     } else {
-      $('#game-result').html('You Got It! Press: Go Again!').removeClass('red').addClass('green');
+      $('#game-result').html('You Got It! Press: Go Again!').attr('class', 'green');
       $('#user-input-answers').html('');
       $('#play-game').html('Go Again!').removeClass('hide');
     }
   });
 
-  //Play game
-  $('#play-game').click(function() {
+  // Play game
+  $('#play-game').on('click', function() {
     display();
-    $('#submit-answer').prop('disabled', false).removeClass('hide');
     $(this).prop('disabled', true).addClass('hide');
+    $('#submit-answer').prop('disabled', false).removeClass('hide');
 
     // Clear data
     $('#user-input-answers, #game-result').html('');
